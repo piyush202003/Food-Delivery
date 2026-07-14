33e361b91cf7f7c7d1a1bf06a211aed49f19b4db
pokemon <pokemon30blade@gmail.com>
2026-07-14 16:40:02 +0530

    updated login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
-def login(request):
-    return render(request, "accounts/login.html")
+def login_view(request):
+    context = {
+        "is_login" : True,
+    }
+    return render(request, "accounts/login.html", context=context)
+
+def register_view(request):
+    context = {
+        "is_login" : False,
+    }
+    return render(request, "accounts/login.html", context = context)
