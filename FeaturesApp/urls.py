c982ccbb5fdc668af243f85f1e26297eb068b7ec
pokemon <pokemon30blade@gmail.com>
2026-07-18 00:11:40 +0530

    added popular products

--- FeaturesApp/urls.py
+++ FeaturesApp/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
     path("", Home, name = "Home"),
     path("products/", Products, name="Products"),
-    path("products/<int:pdid>/", ProductPage, name = "ProductPage"),
+    path("products/<str:pdid>/", ProductPage, name = "ProductPage"),
     path("search/", SearchResults, name="SearchResults"),
     path("deals/", FlashDeals, name = "FlashDeals"),
     path("checkout/", Checkout, name="Checkout"),
