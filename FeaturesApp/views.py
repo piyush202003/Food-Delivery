43f1c80a0c1e80663c03c9b5de9507b740f8b71c
pokemon <pokemon30blade@gmail.com>
2026-07-17 13:58:09 +0530

    Added Categories Scroll Bar

--- FeaturesApp/views.py
+++ FeaturesApp/views.py
@@ -9,6 +9,18 @@ def Home(request):
             "name" : "John Doe", "email" : "john@example.com", "isAdmin" : True
         },
         "cartCount":12,
+        "categoriesData" : [
+            { "slug": "fruits-vegetables", "name": "Fruits & Vegetables", "image": "fruits_vegetables" },
+            { "slug": "personal-care", "name": "Personal Care", "image": "personal_care" },
+            { "slug": "pantry-staples", "name": "Pantry Staples", "image": "pantry_staples" },
+            { "slug": "bakery", "name": "Bakery", "image": "bakery" },
+            { "slug": "beverages", "name": "Beverages", "image": "drinks" },
+            { "slug": "meat-seafood", "name": "Meat & Seafood", "image": "meat_seafood" },
+            { "slug": "snacks", "name": "Snacks", "image": "snacks" },
+            { "slug": "frozen-foods", "name": "Frozen Foods", "image": "frozen_foods" },
+            { "slug": "baby-care", "name": "Baby Care", "image": "baby_care" },
+            { "slug": "dairy-eggs", "name": "Dairy & Eggs", "image": "dairy_eggs" },
+        ],
     }
     return render(request, "Home.html", context=context)
 
